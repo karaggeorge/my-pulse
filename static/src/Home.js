5462fb8e57429bfbef531c154bc98c636f14d760
Karaggeorge <karaggeorge0@gmail.com>
2018-01-20 00:15:52 -0500

    Add correct start script

--- static/src/Home.js
+++ static/src/Home.js
@@ -3,12 +3,6 @@ import React, { Component } from 'react';
 class Home extends Component {
   componentWillMount() {
     console.log(window.location.search);
-    console.log(toObject(parse(window.location.search.substr(1))));
-    const { app, token, err } = toObject(parse(window.location.search.substr(1)));
-    if (app === 'ig' && !err) {
-      console.log('Sending: ', `exp://exp.host/@karaggeorge/mypulse?token=${token}`);
-      window.location = `exp://exp.host/@karaggeorge/mypulse?token=${token}`;
-    }
   }
 
   render() {
